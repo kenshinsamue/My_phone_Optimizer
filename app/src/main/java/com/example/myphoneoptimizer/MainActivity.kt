14bd0e8ea7265882545df83abb33cb988149d31b
k1k4ss0 <predatoren1@gmail.com>
2021-12-02 15:45:24 +0000

    leyendo el fichero lo siguiente es crear un objeto json para guardar la informacion

--- app/src/main/java/com/example/myphoneoptimizer/MainActivity.kt
+++ app/src/main/java/com/example/myphoneoptimizer/MainActivity.kt
@@ -3,38 +3,58 @@ package com.example.myphoneoptimizer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import java.io.BufferedReader
 import java.io.File
 import java.io.InputStream
 
 
+
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.dispositivo).apply {
-            text = android.os.Build.DEVICE
-        }
-        findViewById<TextView>(R.id.modelo).apply {
-            text = android.os.Build.MODEL
-        }
-
-
-        val bufferedReader: BufferedReader = File("/proc/cpuinfo").bufferedReader()
-        val inputString = bufferedReader.use {
-            it.readText()
-        }
-        findViewById<TextView>(R.id.informacion_).apply {
-            text = inputString
-        }
-        println(inputString)
-//            val inputStream:  InputStream = File("/proc/cpuinfo").inputStream()
-//            val inputStream = inputStream.bufferedReader().use{
-//
-//            }
-
-//        info_modelo.textContent = android.os.Build.DEVICE
-
-
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    findViewById<TextView>(R.id.dispositivo).apply {
+      text = android.os.Build.DEVICE
     }
+    findViewById<TextView>(R.id.modelo).apply {
+      text = android.os.Build.MODEL
+    }
+    cpuParser()
+  }
+
+
+  /**
+   * Este metodo sirve para buscar la informacion del CPU del dispositivo buscando el en fichero "/proc/cpuinfo"
+   */
+  private fun cpuParser(){
+    val inputStream: InputStream = File("/proc/cpuinfo").inputStream()
+    val lineList = mutableListOf<String>()
+    val infoCPU = ""
+    inputStream.bufferedReader().useLines {
+      lines -> lines.forEach {
+        lineList.add(it)
+      }
+    }
+    println("---------------------------------->>>>>>>>>>>>")
+    lineList.forEach{
+      dividirLinea(it,":")
+    }
+  }
+
+  /**
+   * Este metodo sirve para leer la linea de un fichero que este delimitado por ":" y devolver esa misma linea en formato JSON
+   * @param linea string de la linea leida del fichero
+   * @param delimitador string del delimitador a encontrar
+   * @return el string listo para ser parseado a JSON
+   */
+  private fun dividirLinea(linea:String, delimitador:String):String{
+    var objeto =""
+    if(linea.length !=0){
+      var pos = linea.indexOf(delimitador)
+      val valor = linea.substring(pos+1)
+      var indice = linea.substring(0,pos-1)
+      objeto ="{"+'"'+indice+'"'+':'+valor+'}'
+    }
+    return objeto
+  }
+
 }
